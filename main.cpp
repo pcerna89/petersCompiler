d9ffa4b6c5809076eda279804f4d8d93ed21e64f
Peter Cerna <petercerna@gmail.com>
2024-03-27 19:16:40 -0500

    brand new beginnings!

--- main.cpp
+++ main.cpp
@@ -1,19 +1,3 @@
 // Created by Peter Cerna
 // main.cpp
 
-#include "InputFile.hpp"
-#include "Tokenizer.hpp"
-#include <iostream>
-
-int main(){
-    string filename = "sampleJava0.java0"; // test file name
-    Tokenizer tokenizer(filename);
-
-    Token token = tokenizer.getNextToken();
-    while (token.type != TokenType::END_OF_FILE){
-        cout << token.toString() << endl; // print our token
-        token = tokenizer.getNextToken(); // retrieve next token
-    }
-
-    return 0;
-}
